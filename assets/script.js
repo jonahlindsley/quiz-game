f5645ae0532ca4dc7c167bcd087b869559f1d10e
jonah lindsley <jonahlindsley@yahoo.com>
2022-07-14 20:25:57 -0500

    improved JS to allow the quiz to restart and added questions

--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,32 @@
 var quizMe = document.querySelector("#takeQuiz");
 var dontQuizMe = document.querySelector("#dontQuiz");
 var pastScore = JSON.parse(localStorage.getItem('highscore'))
-var that = pastScore.value
-console.log(pastScore.name)
-// counter for the number we're on that corrisponds to the array of question and answers
-
     var counts = 0;
     var startMe = document.querySelector('#start')
     var counterEl = document.querySelector('#counter')
     var listEl = document.querySelector('#list')
-    
-    
     var finalWrapper = document.querySelector('#wrapper')
-   
-
     var score = 0;
     var timerEl = document.getElementById('countdown');
     var wrapperEl = document.querySelector('#Q1A');
 
-//use the code provided in the avtivity, the local storage accepts 2 args, the class(what you name it) and the value(whats actually stored)
-
 var html = `
-<h2 id="q1"> this is the first question</h2>
-<button id="notRight1" class="btn" >answer2</button>        
-<button id="answer1a" class="btn">right answer</button>
-<button id="notRight2" class="btn">answer3</button>`;
+<h2 id="q1"> How do you write "Hello World" in an alert box?</h2>
+<button id="notRight1" class="btn" >msgBox('hello world')</button>        
+<button id="answer1a" class="btn">alert('hello world')</button>
+<button id="notRight2" class="btn">alertBox('hello world')</button>`;
 
 var html2 = `
-<h2 id="q2"> this is the second question</h2>
-<button id="answer2b" class="btn">right answer</button> 
-<button id="notRight3" class="btn">answer 1</button>
-<button id="notRight4" class="btn">answer3</button>`
+<h2 id="q2"> How do you call a function named "myFunction"?</h2>
+<button id="answer2b" class="btn">myFunction()</button> 
+<button id="notRight3" class="btn">call function myFunction</button>
+<button id="notRight4" class="btn">call myFunction()</button>`
 
 var html3 = `
-<h2 id="q3"> this is the third question</h2>
-<button id="notRight5" class="btn">answer 1</button>
-<button id="notRight6" class="btn">answer3</button>
-<button id="answer3c" class="btn">right answer</button>`
-
-var html4 = `
-<section id ="finalResults" class="box-style">
-  <input type="text" class="form-control" id="input"placeholder="enter your initals to log your score" aria-label="Recipient's username" aria-describedby="button-addon2">
-  <button class="btn" id="submit">submit your score</button>
-<button id="restart">would you like you play again?</button>
-</section>
-`
-var html5 = `
-<div class="box-style" id="lastPage">
-<p>name ${pastScore.name} score ${pastScore.score}</p>
-<button id="clearScores">clear highscores</button>
-<button id="restart">would you like you play again?</button>
-</div>`
-
+<h2 id="q3"> How to write an IF statement in JavaScript?</h2>
+<button id="notRight5" class="btn">if i === 5</button>
+<button id="notRight6" class="btn">if i = 5 then</button>
+<button id="answer3c" class="btn">if (i == 5)</button>`
 
 var html6 = `
 <button id="start" class="btn">start quiz</button>`
@@ -62,19 +37,7 @@ function displayMessage() {
     console.log('no more time')
     var wordCount = 0;
 }
-var timeLeft = 30;
-
-
-
-
-
-
-
-
-
-//to create a new varaibale thats set to the final score and the user iniitals, create a new div, 2 buttons, one fore clearing the score and
-//one to restart the quiz
-
+var timeLeft = 10;
 
 
 //this is the main function
@@ -85,8 +48,6 @@ function runQuiz() {
 
     document.getElementById('start').remove();
 
-
-    
     //adds the second round buttons and deletes the first ones
     function change1(){
     document.body.insertAdjacentHTML('beforeend', html2)
@@ -107,28 +68,41 @@ function runQuiz() {
         }
         
         function change3(){
+            
+            var html4 = `
+            <section id ="finalResults" class="box-style">
+            <h2 id="finalMessage">your score is ${score} good job!</h2>
+              <input type="text" class="form-control" id="input"placeholder="enter your initals to log your score" aria-label="Recipient's username" aria-describedby="button-addon2">
+              <button class="btn" id="submit">submit your score</button>
+            <button id="restart">would you like you play again?</button>
+            </section>
+            `
             finalWrapper.insertAdjacentHTML('beforeend', html4,)
-            var finalBox = document.querySelector('#finalResults')
-            finalBox.insertAdjacentHTML('afterbegin', `<h2 id="finalMessage">your score is ${score} good job!</h2>`)
             document.getElementById('q3').remove();
             document.getElementById('notRight5').remove();
             document.getElementById('notRight6').remove();
             document.getElementById('answer3c').remove();
             var startAgain = document.querySelector('#restart')
+            var subBtn = document.getElementById('submit')
             var input = document.querySelector('#input')
-            startAgain.addEventListener("click", runQuiz2)
-            var subBtn  = document.querySelector('#submit')
-            subBtn.addEventListener('click', subScores)
-            }
-            function change4(){
+            startAgain.addEventListener('click', runQuiz2)
+            subBtn.addEventListener('click', function(){
+                var html5 = `
+                <div class="box-style" id="lastPage">
+                <h2>${input.value} your latest score is ${score}</h2>
+                <button id="restart">would you like to play again?</button>
+                </div>`
                 finalWrapper.insertAdjacentHTML('beforeend', html5)
+                subScores()
                 var startAgain = document.querySelector('#restart')
-                startAgain.addEventListener("click", runQuiz2)
-            }
-            
-    
+                startAgain.addEventListener("click", function(){
+                    document.getElementById('lastPage').remove();
+                    runQuiz2()
+                })
 
-            
+            })
+
+            }
 
     //inserts the html to make first buttons
     document.body.insertAdjacentHTML('beforeend', html)
@@ -137,7 +111,9 @@ function runQuiz() {
     var right1 = document.querySelector('#answer1a') 
     var notRight1 = document.querySelector('#notRight1')
     var notRight2 = document.querySelector('#notRight2')
-
+    var indicator = document.getElementById('indicator')
+    indicator.value = 1
+    console.log(indicator.value)
     //correct answer
     right1.addEventListener("click", function(){
         score++
@@ -164,7 +140,9 @@ function runQuiz() {
     var right2 = document.querySelector('#answer2b') 
     var notRight3 = document.querySelector('#notRight3')
     var notRight4 = document.querySelector('#notRight4')
-
+    var indicator = document.getElementById('indicator')
+    indicator.value ++
+    console.log(indicator.value)
     //second round of questions
     right2.addEventListener("click", function(){
         score++
@@ -188,11 +166,14 @@ function runQuiz() {
 
 
 function thirdRound(){
+
         //the variables for the third round buttons
         var right3 = document.querySelector('#answer3c') 
         var notRight5 = document.querySelector('#notRight5')
         var notRight6 = document.querySelector('#notRight6')
-
+        var indicator = document.getElementById('indicator')
+        indicator.value ++
+        console.log(indicator.value)
     // third round of questions
     right3.addEventListener("click", function(){
         score++
@@ -218,10 +199,6 @@ function thirdRound(){
         timerEl.textContent = ``;
         change3()
     })
-
-
-
-
 }
 
 function runQuiz2(){
@@ -239,12 +216,11 @@ var highScore = {
     name: input.value,
     score: score,}
 localStorage.setItem('highscore', JSON.stringify(highScore));
-document.getElementById('finalResults').remove();
-finalWrapper.insertAdjacentHTML('beforeend', html5)
-var startAgain = document.querySelector('#restart')
-startAgain.addEventListener("click", runQuiz2)
+document.getElementById('input').remove();
+document.getElementById('restart').remove();
+document.getElementById('submit').remove();
+document.getElementById('finalMessage').remove();
 }
-    
     var timeInterval = setInterval(function () {
       timeLeft--;
       console.log();
@@ -254,8 +230,20 @@ startAgain.addEventListener("click", runQuiz2)
         clearInterval(timeInterval);
         timerEl.textContent = ``;
         alert('you have run out of time!')
+        // location.reload();
+        console.log(indicator.value)
+    if (indicator.value == 1){
+        change1()
+        change2()
+        change3()
+    }else if (indicator.value == 2){
+        change2()
+        change3()
+    }else{
+        change3()
+    }
+        
       }
      
     }, 1000);
   }
-  
